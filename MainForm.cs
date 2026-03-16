2850728334cb3e2a55cd42cefbd47e142d9175f0
Eduardo Almeida <145823210+Dartin17@users.noreply.github.com>
2026-03-16 17:19:13 -0300

    fix: consertado bug visual com o preview quando clicava nos botões

--- MainForm.cs
+++ MainForm.cs
@@ -140,34 +140,36 @@ namespace paint_cg
 
         private void panelDraw_MouseDown(object sender, MouseEventArgs e)
         {
-            if (e.Button != MouseButtons.Left)
-                return;
-
-            if (EstaNoModoPrimitivas)
+            if (e.Button == MouseButtons.Left)
             {
-                if (freeDrawMode)
-                {
-                    isDrawing = true;
-                    lastPoint = e.Location;
-                }
-                else
-                {
-                    p1 = e.Location;
-                    p2 = e.Location;
-                    isPreviewingPrimitive = true;
-                }
-            }
-            else if (EstaNoModoPoligonos)
-            {
-                if (isDrawingPolygon && currentPolygon != null)
-                {
-                    currentPolygon.Points.Add(new PointData(e.Location));
 
-                    pointList = currentPolygon.Points;
-                    dataGridViewPoints.DataSource = null;
-                    dataGridViewPoints.DataSource = pointList;
+                if (EstaNoModoPrimitivas)
+                {
+                    if (freeDrawMode)
+                    {
+                        isDrawing = true;
+                        lastPoint = e.Location;
+                    }
+                    else
+                    {
+                        p1 = e.Location;
+                        p2 = e.Location;
+                        isPreviewingPrimitive = true;
+                    }
+                }
+                else if (EstaNoModoPoligonos)
+                {
+                    if (isDrawingPolygon && currentPolygon != null)
+                    {
+                        currentPolygon.Points.Add(new PointData(e.Location));
 
-                    panelDraw.Invalidate();
+                        pointList = currentPolygon.Points;
+                        dataGridViewPoints.DataSource = null;
+                        dataGridViewPoints.DataSource = pointList;
+
+                        RedrawCanvas();
+                        panelDraw.Invalidate();
+                    }
                 }
             }
         }
@@ -197,41 +199,57 @@ namespace paint_cg
             else if (EstaNoModoPoligonos)
             {
                 mousePositionPolygon = e.Location;
-                panelDraw.Invalidate();
+
+                if (isDrawingPolygon)
+                {
+                    panelDraw.Invalidate();
+                }
             }
         }
 
+
         private void panelDraw_MouseUp(object sender, MouseEventArgs e)
         {
-            if (e.Button != MouseButtons.Left)
-                return;
-
-            if (EstaNoModoPrimitivas)
+            if (e.Button == MouseButtons.Left)
             {
-                if (freeDrawMode)
+                if (EstaNoModoPrimitivas)
                 {
-                    isDrawing = false;
-                }
-                else
-                {
-                    if (isPreviewingPrimitive)
+                    if (freeDrawMode)
                     {
-                        p2 = e.Location;
-                        isPreviewingPrimitive = false;
-
-                        DrawPrimitive(canvas, Color.Black);
-
-                        if (previewCanvas != null)
+                        if (isDrawing)
                         {
-                            previewCanvas.Dispose();
-                            previewCanvas = null;
+                            isDrawing = false;
                         }
+                    }
+                    else
+                    {
+                        if (isPreviewingPrimitive)
+                        {
+                            p2 = e.Location;
+                            isPreviewingPrimitive = false;
 
+                            DrawPrimitive(canvas, Color.Black);
+
+                            if (previewCanvas != null)
+                            {
+                                previewCanvas.Dispose();
+                                previewCanvas = null;
+                            }
+
+                            panelDraw.Invalidate();
+                        }
+                    }
+                }
+                else if (EstaNoModoPoligonos)
+                {
+                    if (isDrawingPolygon)
+                    {
                         panelDraw.Invalidate();
                     }
                 }
             }
         }
+
 
         private void DrawPrimitive(Bitmap target, Color color)
         {
@@ -269,62 +287,83 @@ namespace paint_cg
         }
 
 
-        private void DrawPolygonPreview(Graphics g, Polygon polygon)
+        private void DrawVertex(Bitmap target, Point p, Color color)
         {
-            if (polygon.Points.Count == 0)
-                return;
-
-            using Bitmap temp = (Bitmap)canvas.Clone();
-
-            for (int i = 0; i < polygon.Points.Count - 1; i++)
+            for (int dx = -3; dx <= 3; dx++)
             {
-                Point a = polygon.Points[i].ToPoint();
-                Point b = polygon.Points[i + 1].ToPoint();
-                Primitives.LineMidpoint(temp, a, b, Color.Black);
+                for (int dy = -3; dy <= 3; dy++)
+                {
+                    int x = p.X + dx;
+                    int y = p.Y + dy;
+
+                    if (x >= 0 && x < target.Width && y >= 0 && y < target.Height)
+                    {
+                        target.SetPixel(x, y, color);
+                    }
+                }
             }
+        }
 
-            Point last = polygon.Points[polygon.Points.Count - 1].ToPoint();
-            Primitives.LineMidpoint(temp, last, mousePositionPolygon, Color.Gray);
+        private void DrawPolygonOnBitmap(Bitmap target, Polygon polygon, Color color, bool closePolygon)
+        {
+            int currentTotalPoints = polygon.Points.Count;
 
-            foreach (var point in polygon.Points)
+            if (currentTotalPoints > 0)
             {
-                Point p = point.ToPoint();
-                using Graphics gt = Graphics.FromImage(temp);
-                gt.FillEllipse(Brushes.Black, p.X - 3, p.Y - 3, 6, 6);
-            }
+                for (int i = 0; i < currentTotalPoints; i++)
+                {
+                    DrawVertex(target, polygon.Points[i].ToPoint(), color);
+                }
 
-            g.DrawImageUnscaled(temp, 0, 0);
+                for (int i = 0; i < currentTotalPoints - 1; i++)
+                {
+                    Point p1 = polygon.Points[i].ToPoint();
+                    Point p2 = polygon.Points[i + 1].ToPoint();
+                    Primitives.LineMidpoint(target, p1, p2, color);
+                }
+
+                if (closePolygon && currentTotalPoints >= 3)
+                {
+                    Point first = polygon.Points[0].ToPoint();
+                    Point last = polygon.Points[currentTotalPoints - 1].ToPoint();
+                    Primitives.LineMidpoint(target, last, first, color);
+                }
+            }
         }
 
         private void RedrawCanvas()
         {
-            g.Clear(Color.White);
+            if (canvas == null)
+                return;
 
-            foreach (var polygon in polygonList)
+            using (Graphics gx = Graphics.FromImage(canvas))
+            {
+                gx.Clear(Color.White);
+            }
+
+            foreach (Polygon polygon in polygonList)
             {
                 Color color = polygon == selectedPolygon ? Color.Blue : Color.Black;
+                bool isClosed = polygon != currentPolygon || !isDrawingPolygon;
 
-                if (polygon.Points.Count == 1)
-                {
-                    Point p = polygon.Points[0].ToPoint();
-                    g.FillEllipse(color == Color.Blue ? Brushes.Blue : Brushes.Black, p.X - 3, p.Y - 3, 6, 6);
-                }
-                else
-                {
-                    for (int i = 0; i < polygon.Points.Count - 1; i++)
-                    {
-                        Point a = polygon.Points[i].ToPoint();
-                        Point b = polygon.Points[i + 1].ToPoint();
-                        Primitives.LineMidpoint(canvas, a, b, color);
-                    }
+                DrawPolygonOnBitmap(canvas, polygon, color, isClosed);
+            }
+        }
 
-                    if (polygon.Points.Count >= 3)
-                    {
-                        Point first = polygon.Points[0].ToPoint();
-                        Point last = polygon.Points[polygon.Points.Count - 1].ToPoint();
-                        Primitives.LineMidpoint(canvas, last, first, color);
-                    }
-                }
+        private void BuildPolygonPreview()
+        {
+            if (previewCanvas != null)
+            {
+                previewCanvas.Dispose();
+                previewCanvas = null;
+            }
+
+            previewCanvas = (Bitmap)canvas.Clone();
+
+            if (isDrawingPolygon && currentPolygon != null && currentPolygon.Points.Count > 0)
+            {
+                Point last = currentPolygon.Points[currentPolygon.Points.Count - 1].ToPoint();
+                Primitives.LineMidpoint(previewCanvas, last, mousePositionPolygon, Color.Gray);
             }
         }
 
@@ -344,12 +383,21 @@ namespace paint_cg
                     DrawPrimitive(previewCanvas, Color.Gray);
                     e.Graphics.DrawImageUnscaled(previewCanvas, 0, 0);
                 }
+                else
+                {
+                    e.Graphics.DrawImageUnscaled(canvas, 0, 0);
+                }
             }
             else if (EstaNoModoPoligonos)
             {
-                if (isDrawingPolygon && currentPolygon != null)
+                if (isDrawingPolygon && currentPolygon != null && currentPolygon.Points.Count > 0)
                 {
-                    DrawPolygonPreview(e.Graphics, currentPolygon);
+                    BuildPolygonPreview();
+                    e.Graphics.DrawImageUnscaled(previewCanvas, 0, 0);
+                }
+                else
+                {
+                    e.Graphics.DrawImageUnscaled(canvas, 0, 0);
                 }
             }
         }
@@ -383,18 +431,23 @@ namespace paint_cg
             if (!EstaNoModoPoligonos)
                 return;
 
+
             currentPolygon = new Polygon
             {
                 Name = $"Polígono {polygonCounter++}"
             };
 
+            polygonList.Add(currentPolygon);
+            selectedPolygon = currentPolygon;
             isDrawingPolygon = true;
-            selectedPolygon = null;
+
+            listBoxPolygons.SelectedItem = currentPolygon;
 
             pointList = currentPolygon.Points;
             dataGridViewPoints.DataSource = null;
             dataGridViewPoints.DataSource = pointList;
 
+            RedrawCanvas();
             panelDraw.Invalidate();
         }
 
@@ -409,12 +462,17 @@ namespace paint_cg
                 return;
             }
 
-            polygonList.Add(currentPolygon);
             selectedPolygon = currentPolygon;
             listBoxPolygons.SelectedItem = currentPolygon;
 
             isDrawingPolygon = false;
             currentPolygon = null;
+
+            if (previewCanvas != null)
+            {
+                previewCanvas.Dispose();
+                previewCanvas = null;
+            }
 
             RedrawCanvas();
             panelDraw.Invalidate();
@@ -422,28 +480,44 @@ namespace paint_cg
 
         private void buttonDeletePolygon_Click(object sender, EventArgs e)
         {
-            if (!EstaNoModoPoligonos)
-                return;
-
-            if (listBoxPolygons.SelectedItem is Polygon polygon)
+            if (EstaNoModoPoligonos)
             {
-                polygonList.Remove(polygon);
-                selectedPolygon = null;
 
-                pointList = new BindingList<PointData>();
-                dataGridViewPoints.DataSource = null;
-                dataGridViewPoints.DataSource = pointList;
 
-                RedrawCanvas();
-                panelDraw.Invalidate();
+                if (listBoxPolygons.SelectedItem is Polygon polygon)
+                {
+                    if (polygon == currentPolygon)
+                    {
+                        currentPolygon = null;
+                        isDrawingPolygon = false;
+                    }
+
+                    polygonList.Remove(polygon);
+                    selectedPolygon = null;
+
+                    pointList = new BindingList<PointData>();
+                    dataGridViewPoints.DataSource = null;
+                    dataGridViewPoints.DataSource = pointList;
+
+                    if (previewCanvas != null)
+                    {
+                        previewCanvas.Dispose();
+                        previewCanvas = null;
+                    }
+
+                    RedrawCanvas();
+                    panelDraw.Invalidate();
+                }
             }
         }
 
         private void listBoxPolygons_SelectedIndexChanged(object sender, EventArgs e)
         {
-
-
-            if (listBoxPolygons.SelectedItem is Polygon polygon)
+            if (isDrawingPolygon && currentPolygon != null)
+            {
+                listBoxPolygons.SelectedItem = currentPolygon;
+            }
+            else if (listBoxPolygons.SelectedItem is Polygon polygon)
             {
                 selectedPolygon = polygon;
                 pointList = polygon.Points;
